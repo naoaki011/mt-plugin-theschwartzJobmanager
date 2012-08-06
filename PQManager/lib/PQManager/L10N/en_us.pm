f7511c36fbe2a0f02dc5e38f4abe0c918cda6959
naoaki011 <naoaki@orion.ocn.ne.jp>
2012-08-06 13:41:10 +0900

    base of PQManagerPlugin for MT5.x.

---
+++ PQManager/lib/PQManager/L10N/en_us.pm
@@ -0,0 +1,9 @@
+package PQManager::L10N::en_us;
+
+use strict;
+use base 'PQManager::L10N';
+use vars qw( %Lexicon );
+%Lexicon = (
+);
+
+1;
---
+++ PQManager/lib/PQManager/L10N/ja.pm
@@ -0,0 +1,24 @@
+package PQManager::L10N::ja;
+
+use strict;
+use base 'PQManager::L10N::en_us';
+use vars qw( %Lexicon );
+
+%Lexicon = (
+    'Provides a management console for Publish Queue.' => '公開キューを管理する機能を追加します。',
+    'Change Priority' => '優先度を変更する',
+    'You must enter a number between 1 and 10.' => '優先度に入力できる数字は、1から10までの整数です。',
+    'Publish Queue Jobs' => '公開キューのジョブ',
+    'The selected jobs were deleted.' => '選択したジョブは削除されました。',
+    'The selected jobs had their priority set to [_1].' => '選択したジョブの優先度は「[_1]」に変更されました。',
+    'No jobs could be found.' => 'ジョブはありません。',
+    'Pri.' => '優先度',
+    'File Path' => 'ファイルパス',
+    'Inserted' => 'キュー・イン',
+    'Delete Jobs' => 'ジョブを削除する',
+    'Job Description' => 'ジョブの説明',
+    'Remaining Jobs' => 'ジョブ数',
+    'Manage Publish Queue Jobs' => '公開キュージョブを一覧する',
+);
+
+1;
