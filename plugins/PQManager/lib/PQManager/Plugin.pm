1e52c28232c7fcfcf77a1cb6bae4ce78d24d642e
naoaki011 <naoaki@orion.ocn.ne.jp>
2013-07-31 12:18:16 +0900

    readme correct.

--- plugins/PQManager/lib/PQManager/Plugin.pm
+++ plugins/PQManager/lib/PQManager/Plugin.pm
@@ -41,11 +41,13 @@ sub _delete {
     $app->validate_magic
       or return;
 
-    require MT::TheSchwartz::Job;
     my @jobs = $app->param('id');
     for my $job_id (@jobs) {
-        my $job = MT::TheSchwartz::Job->load({jobid => $job_id})
+        my $job = MT->model('ts_job')->load({jobid => $job_id})
           or next;
+        my $funcmap = MT->model('ts_funcmap')->load({funcid => $job->funcid})
+          or next;
+        next unless ($funcmap->funcname eq 'MT::Worker::Publish');
         $job->remove();
     }
     $app->redirect(
@@ -66,15 +68,16 @@ sub _publish {
       or return;
 
     require MT::WeblogPublisher;
-    require MT::TheSchwartz::Job;
-    require MT::FileInfo;
     my @jobs = $app->param('id');
     my $pub = MT::WeblogPublisher->new;
     for my $job_id (@jobs) {
-        my $job = MT::TheSchwartz::Job->load({jobid => $job_id})
+        my $job = MT->model('ts_job')->load({jobid => $job_id})
           or next;
+        my $funcmap = MT->model('ts_funcmap')->load({funcid => $job->funcid})
+          or next;
+        next unless ($funcmap->funcname eq 'MT::Worker::Publish');
         if ( my $key = $job->uniqkey ) {
-            if ( my $finfo = MT::FileInfo->load( $key ) ) {
+            if ( my $finfo = MT->model('fileinfo')->load( $key ) ) {
                 if ( $pub->rebuild_from_fileinfo( $finfo ) ) {
                     $job->remove
                       or die $job->errstr;
@@ -96,18 +99,22 @@ sub _publish {
 
 sub _priority {
     my $app = shift;
-    $app->validate_magic or return;
+    $app->validate_magic
+      or return;
 
     my $pri = $app->param('itemset_action_input');
     if (($pri !~ /^[0-9]+$/)||($pri > 10)) {
         return $app->error("You must enter a number between 1 and 10.");
     }
-    require MT::TheSchwartz::Job;
     my @jobs = $app->param('id');
     for my $job_id (@jobs) {
-        my $job = MT::TheSchwartz::Job->load({jobid => $job_id}) or next;
+        my $job = MT->model('ts_job')->load({jobid => $job_id})
+          or next;
+        my $funcmap = MT->model('ts_funcmap')->load({funcid => $job->funcid})
+          or next;
+        next unless ($funcmap->funcname eq 'MT::Worker::Publish');
         $job->priority($pri);
-    $job->save();
+        $job->save();
     }
     $app->redirect(
         $app->uri(
